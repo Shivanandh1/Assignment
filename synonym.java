7f7d521bc7dc735a7f29d96dc4cc41b26b3fb8c8
Shivanandh <84269948+Shivanandh1@users.noreply.github.com>
2021-10-23 21:15:03 +0530

    Update synonym.java

--- synonym.java
+++ synonym.java
@@ -7,45 +7,73 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Synonym {
-	
-	public static void main(String[] args) throws IOException{
-
-	
-		// creating a My HashTable Dictionary
-		Hashtable<String, String> synonym  = new Hashtable<String, String>();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("To store a synonym");
-		System.out.println("Enter a word ");
-		String name = br.readLine();
-		System.out.println("Enter Synonym");
-		String sy=br.readLine();
-		
-		
-		//System.out.println(synonym);
-		BufferedWriter bw = null;
-		try {
-			bw = new BufferedWriter(new FileWriter("emps.txt", true));
-			bw.write("Name is :" + name+"\n");
-			bw.newLine();
-			bw.write("synonym is : "+sy+"\n");
-			bw.newLine();
-		} catch (IOException e) {
-			e.printStackTrace(); 
-		} finally {
-			bw.close();
-		}
-			synonym.put(name, sy);	
-		    //System.out.println("synonym is :" + synonym.get(name));
-		    System.out.println("Please enter a word to look for synonym");
-		    String word=br.readLine();
-		    System.out.println(synonym.get(word));
-		 
-		
-		    
+class add{
+	 public void createFile() throws IOException {
+	 
+BufferedWriter bw = null;
+try {
+	bw = new BufferedWriter(new FileWriter("emps.txt", true));
+//	String name = null;
+//	bw.write("Name is :" + name+"\n");
+//	bw.newLine();
+//	String sy = null;
+//	bw.write("synonym is : "+sy+"\n");
+//	bw.newLine();
+} catch (IOException e) {
+	e.printStackTrace(); 
+} finally {
+	bw.close();
+}
+        HashMap<String, String> synonym  = new HashMap<String, String>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+         System.out.println("To store a synonym");
+          System.out.println("Enter a word ");
+          String name = br.readLine();
+          System.out.println("Enter Synonym");
+          String sy=br.readLine();
+          synonym.put(name, sy);	
+          System.out.println("Please enter a word to look for synonym");
+          String word=br.readLine();
+          String a=synonym.get(word);
+          System.out.println("synonym is :"+word+" "+a);
+        }
+}
+}
+class codeExit{
+	public void exit(){
+		System.out.println("Successfully exited");
+		System.exit(0);
 	}
 }
 
+public class Synonym {
+	
+	public static void main(String[] args) throws IOException{
+                     
+		// creating a My HashTable Dictionary
+		//Hashtable<String, String> synonym  = new Hashtable<String, String>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		    int i=0;
+		    while(i<3) {
+		    	System.out.println("please enter choice");
+		    	i=Integer.parseInt(br.readLine());
+		    	switch(i) {
+		    	case 1:
+		    		add obj1=new add();
+		    		obj1.createFile();
+		    	case 0:
+		    		codeExit ce=new codeExit();
+		    		ce.exit();
+		    		System.out.println("Successfully exited");
+		    	}
+		    	if(i>2) {
+		    		System.out.println("not possible");
+		    	}
+		    }
+	}
+
+}
 
 		
 		
